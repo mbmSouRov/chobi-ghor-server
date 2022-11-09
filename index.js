7a99c1097fdee776e436a9cf3fa259f1ca30e168
Murshid Bin Masud Sourov <61899629+mbmSouRov@users.noreply.github.com>
2022-11-10 01:43:51 +0600

    all routes created. jwt remains

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -21,6 +21,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("chobiGhor").collection("services");
+    const reviewsCollection = client.db("chobiGhor").collection("reviews");
 
     //ONLY 3 SERVICES
     app.get("/services", async (req, res) => {
@@ -29,13 +30,84 @@ async function run() {
       const services = await cursos.limit(3).toArray();
       res.send(services);
     });
-
     //ALL SERVICE
     app.get("/allServices", async (req, res) => {
       const query = {};
       const cursos = serviceCollection.find(query);
       const services = await cursos.toArray();
       res.send(services);
+    });
+
+    //ALL REVIEW
+    app.get("/allReview", async (req, res) => {
+      const query = {};
+      const cursos = reviewsCollection.find(query);
+      const services = await cursos.toArray();
+      res.send(services);
+    });
+
+    //Find A Single Service
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //Find A Single Review
+    app.get("/allReview/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await reviewsCollection.findOne(query);
+      res.send(service);
+    });
+
+    //reviews add
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result);
+    });
+
+    //service add
+    app.post("/service", async (req, res) => {
+      const review = req.body;
+      const result = await serviceCollection.insertOne(review);
+      res.send(result);
+    });
+
+    //For each service, reviews extract
+    app.get("/allReviews", async (req, res) => {
+      let query = {};
+      if (req.query.service) {
+        query = {
+          service: req.query.service,
+        };
+      }
+      const cursor = reviewsCollection.find(query);
+      const allReviews = await cursor.toArray();
+      res.send(allReviews);
+    });
+
+    //For each user, reviews extract
+    app.get("/userReviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewsCollection.find(query);
+      const allReviews = await cursor.toArray();
+      res.send(allReviews);
+    });
+
+    //DELETE a users review comment
+    app.delete("/allReview/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
   }
